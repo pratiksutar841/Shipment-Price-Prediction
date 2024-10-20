0e416055ae9afc0c314c78619cb73d2eeb6f7c5c
pratiksutar841 <pratiksutar841@gmail.com>
2024-10-20 20:52:57 +0530

    ShipmentPrediction

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="shipment",
+    version="0.0.1",
+    author="Pratik",
+    author_email="pratiksutar841@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
